fb0d606cd0d919bda33435f7d6dbe445c193ada6
ntestdan <ntestdan@gmail.com>
2017-07-30 22:43:16 +0300

    Extended StudentDto class
    implemented using java.lang.Comparable and java.util.Comparator

--- src/main/java/StudentDto.java
+++ src/main/java/StudentDto.java
@@ -1,9 +1,11 @@
+import java.util.Comparator;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class StudentDto {
+public class StudentDto implements Comparable<StudentDto> {
 
     private int id;
     private String name;
@@ -63,5 +65,45 @@ public class StudentDto {
     public void setCourse(int course) {
         this.course= course;
     }
+
+	
+    public int compareTo(StudentDto sd) {
+		int compareisionByName = this.name.compareTo(sd.name);
+
+	    if( compareisionByName != 0 )
+	    {
+	        //sort by name
+	        return compareisionByName;
+	    }
+	    else
+	    {
+	        //name equal, sort by age
+	        //return this.age.compareTo(sd.age);
+	    	 return this.age < sd.age ? -1 : this.age == sd.age ? 0 : 1;
+	    }
+	}
+    
+    public static Comparator<StudentDto> CourseNameComparator = new Comparator<StudentDto>() {
+
+    	public int compare(StudentDto student1, StudentDto student2) {
+    		
+    		
+    		int student1Course = student1.getCourse();
+    		int student2Course = student2.getCourse();
+    		
+    		int compareisionByCourse = student1Course < student2Course ? -1 : student1Course == student2Course ? 0 : 1;
+
+    		if( compareisionByCourse != 0 )
+    	    {
+    	        //sort by name
+    	        return compareisionByCourse;
+    	    }
+    	    else
+    	    {
+    	        //name equal, sort by age
+    	        return student1.compareTo(student2);
+    	    }
+    	}
+    };
 
 }
