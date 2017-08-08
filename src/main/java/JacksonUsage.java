9d91e695194918dcb3e87f01d40119280fe4805d
liliia <LilKonyushenko@gmail.com>
2017-08-08 23:56:10 +0300

    added try catch

--- src/main/java/JacksonUsage.java
+++ src/main/java/JacksonUsage.java
@@ -1,10 +1,10 @@
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -12,51 +12,59 @@ import java.util.Collections;
 import java.util.List;
 
 public class JacksonUsage {
-    public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
-        InputStream input = new FileInputStream("src/students.json");
+
+    public static final String INPUT_FILE = "src/students.json";
+
+    public static void main(String[] args) {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        objectMapper
-                .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+        objectMapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
         TypeFactory typeFactory = TypeFactory.defaultInstance();
-        List<StudentDto> students = objectMapper.readValue(input,
-                typeFactory.constructCollectionType(
-                        ArrayList.class, StudentDto.class));
+        try {
+            InputStream input = new FileInputStream(INPUT_FILE);
+            List<StudentDto> students = objectMapper.readValue(input, typeFactory.constructCollectionType(ArrayList.class, StudentDto.class));
 
-        System.out.println("Student: " + students.get(0).getName() + ", Age: " + students.get(0).getAge()+
-                ", Course: " + students.get(0).getCourse());
-        System.out.println("Student: " + students.get(1).getName() + ", Age: " + students.get(1).getAge() +
-                ", Course: " + students.get(1).getCourse());
-        
-        SortAndOutputStudents(students);
+            System.out.println("Student: " + students.get(0).getName() + ", Age: " + students.get(0).getAge() +
+                    ", Course: " + students.get(0).getCourse());
+            System.out.println("Student: " + students.get(1).getName() + ", Age: " + students.get(1).getAge() +
+                    ", Course: " + students.get(1).getCourse());
+
+            SortAndOutputStudents(students);
+        } catch (FileNotFoundException ex) {
+            System.out.println("File " + INPUT_FILE + " not found");
+        } catch (JsonParseException ex) {
+            System.out.println("Parse exception");
+        } catch (IOException ex) {
+            System.out.println("IO exception");
+        }
     }
-    
-    public static void SortAndOutputStudents(List<StudentDto> students){
-    	//before sort
-    	System.out.println();
-    	System.out.println("List is not sorted");
-      	for(StudentDto student: students){
-      		System.out.println("Student: " + student.getName() + ", Age: " + student.getAge()+
-      	            ", Course: " + student.getCourse());
-      	}
-        
+
+    public static void SortAndOutputStudents(List<StudentDto> students) {
+        //before sort
+        System.out.println();
+        System.out.println("List is not sorted");
+        for (StudentDto student : students) {
+            System.out.println("Student: " + student.getName() + ", Age: " + student.getAge() +
+                    ", Course: " + student.getCourse());
+        }
+
         Collections.sort(students);
-        
+
         System.out.println();
         System.out.println("List is sorted by dafault (Name then Age)");
-      	for(StudentDto student: students){
-      		System.out.println("Student: " + student.getName() + ", Age: " + student.getAge()+
-      	            ", Course: " + student.getCourse());
-      	}
-      	
-      	Collections.sort(students, StudentDto.CourseNameComparator);
-      	
-      	System.out.println();
-      	System.out.println("List is sorted by Course then default sorting");
-      	for(StudentDto student: students){
-      		System.out.println("Student: " + student.getName() + ", Age: " + student.getAge()+
-      	            ", Course: " + student.getCourse());
-      	}
+        for (StudentDto student : students) {
+            System.out.println("Student: " + student.getName() + ", Age: " + student.getAge() +
+                    ", Course: " + student.getCourse());
+        }
+
+        Collections.sort(students, StudentDto.CourseNameComparator);
+
+        System.out.println();
+        System.out.println("List is sorted by Course then default sorting");
+        for (StudentDto student : students) {
+            System.out.println("Student: " + student.getName() + ", Age: " + student.getAge() +
+                    ", Course: " + student.getCourse());
+        }
 
     }
 }
